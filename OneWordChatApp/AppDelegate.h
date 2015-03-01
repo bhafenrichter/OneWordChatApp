a4a8b01480994029677dce58c51856d05b2f0c9e
Brandon Hafenrichter <bhafenri@Brandons-MacBook-Pro.local>
2015-03-01 14:10:30 -0600

    Initial Commit

---
+++ OneWordChatApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneWordChatApp
+//
+//  Created by Brandon Hafenrichter on 3/1/15.
+//  Copyright (c) 2015 Brandon Hafenrichter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
